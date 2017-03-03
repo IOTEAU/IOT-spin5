9c9b6509a101eb85724b5a2883a9e2e64346c016
Teerasak Sathu <maxz_drum@icloud.com>
2017-03-03 14:35:39 +0700

    แก้ REF

--- js/return.js
+++ js/return.js
@@ -1,22 +1,22 @@
 
 var dbRef =firebase.database();
 
-   var btn1 = dbRef.ref('lamp1');
+   var btn1 = dbRef.ref('Lamp1');
       btn1.on('value', function(snapshot) {
         console.log("lamp1:"+snapshot.val());
         document.querySelector("#lampface > input").checked = snapshot.val();
 });
-       var btn2 = dbRef.ref('lamp2');
+       var btn2 = dbRef.ref('Lamp2');
       btn2.on('value', function(snapshot) {
         console.log("lamp2:"+snapshot.val());
-        document.querySelector("#lampnext > input").checked = snapshot.val();
+        document.querySelector("#Lampnext > input").checked = snapshot.val();
 });
-       var btn3 = dbRef.ref('lamp3');
+       var btn3 = dbRef.ref('Lamp3');
       btn3.on('value', function(snapshot) {
         console.log("lamp3:"+snapshot.val());
         document.querySelector("#lampnext1 > input").checked = snapshot.val();
 });
-       var btn4 = dbRef.ref('lamp4');
+       var btn4 = dbRef.ref('Lamp4');
       btn4.on('value', function(snapshot) {
         console.log("lamp4:"+snapshot.val());
         document.querySelector("#lampnext2 > input").checked = snapshot.val();
